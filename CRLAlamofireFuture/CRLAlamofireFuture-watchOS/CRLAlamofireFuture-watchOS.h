33eb55ba08ace2f17800535c07d3a26ccabf1497
RuslaYupyn <yupynruslanevgenovich@gmail.com>
2016-07-04 18:49:09 +0300

    added framework target for watchOS carthage implementation;

---
+++ CRLAlamofireFuture/CRLAlamofireFuture-watchOS/CRLAlamofireFuture-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  CRLAlamofireFuture-watchOS.h
+//  CRLAlamofireFuture-watchOS
+//
+//  Created by Ruslan Yupyn on 7/4/16.
+//  Copyright © 2016 Crossroad Labs. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for CRLAlamofireFuture-watchOS.
+FOUNDATION_EXPORT double CRLAlamofireFuture_watchOSVersionNumber;
+
+//! Project version string for CRLAlamofireFuture-watchOS.
+FOUNDATION_EXPORT const unsigned char CRLAlamofireFuture_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRLAlamofireFuture_watchOS/PublicHeader.h>
+
+
