ee29590d7b5ee3b6f823fab3bc6844d9c3c99286
RuslaYupyn <yupynruslanevgenovich@gmail.com>
2016-07-04 15:19:22 +0300

    added Empty initial framework project

---
+++ CRLAlamofireFuture/CRLAlamofireFuture/CRLAlamofireFuture.h
@@ -0,0 +1,19 @@
+//
+//  CRLAlamofireFuture.h
+//  CRLAlamofireFuture
+//
+//  Created by Ruslan Yupyn on 7/4/16.
+//  Copyright © 2016 Crossroad Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRLAlamofireFuture.
+FOUNDATION_EXPORT double CRLAlamofireFutureVersionNumber;
+
+//! Project version string for CRLAlamofireFuture.
+FOUNDATION_EXPORT const unsigned char CRLAlamofireFutureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRLAlamofireFuture/PublicHeader.h>
+
+
