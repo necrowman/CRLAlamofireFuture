ae01ad19d8d5baac04a3e29777e285d199866a32
RuslaYupyn <yupynruslanevgenovich@gmail.com>
2016-07-06 17:20:54 +0300

    deleted runscripts;

---
+++ Examples/SimpleTvOSCarthage/Carthage/Checkouts/CRLAlamofireFuture/Examples/SimpleTvOSCarthage/Carthage/Checkouts/CRLAlamofireFuture/Examples/SimpleIOSCarthage/Carthage/Checkouts/CRLAlamofireFuture/Examples/SimpleIOSCarthage/Carthage/Checkouts/CRLAlamofireFuture/Examples/SimpleOSXCarthage/Carthage/Checkouts/CRLAlamofireFuture/Examples/SimpleIOSCarthage/Carthage/Checkouts/CRLAlamofireFuture/Sources/CRLAlamofireFuture.swift
@@ -0,0 +1,19 @@
+//
+//  CRLAlamofireFuture.swift
+//  CRLAlamofireFuture
+//
+//  Created by Ruslan Yupyn on 7/4/16.
+//  Copyright © 2016 Crossroad Labs. All rights reserved.
+//
+
+import Foundation
+
+public class CRLAlamofireFuture {
+    
+    public static let instance = CRLAlamofireFuture()
+    
+    public func frameworkFunctionExample() -> String {
+        return "This is test framework function example"
+    }
+    
+}
